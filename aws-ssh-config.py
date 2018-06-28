b6c8644ee6791beb44c2184a19d0c51259f3dcf6
Thaddeus Gholston <thaddeus.gholston@influencehealth.com>
2018-06-27 22:49:45 -0400

    refactoring to use boto3 and updating to allow customizable hostname with replacement and substitutions

--- aws-ssh-config.py
+++ aws-ssh-config.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
 import argparse
+import boto3
+import inflection
 import re
-import sys
-import time
-import boto.ec2
-
+from botocore.exceptions import ProfileNotFound
 
 AMI_NAMES_TO_USER = {
     'amzn' : 'ec2-user',
@@ -28,36 +27,7 @@ BLACKLISTED_REGIONS = [
     'us-gov-west-1'
 ]
 
-
-def generate_id(instance, tags_filter, region):
-    instance_id = ''
-
-    if tags_filter is not None:
-        for tag in tags_filter.split(','):
-            value = instance.tags.get(tag, None)
-            if value:
-                if not instance_id:
-                    instance_id = value
-                else:
-                    instance_id += '-' + value
-    else:
-        for tag, value in instance.tags.items():
-            if not tag.startswith('aws'):
-                if not instance_id:
-                    instance_id = value
-                else:
-                    instance_id += '-' + value
-
-    if not instance_id:
-        instance_id = instance.id
-
-    if region:
-        instance_id += '-' + instance.placement
-
-    return instance_id
-
-
-def main():
+def build_argument_parser(): 
     parser = argparse.ArgumentParser()
     parser.add_argument('--default-user', help='Default ssh username to use if it can\'t be detected from AMI name')
     parser.add_argument('--keydir', default='~/.ssh/', help='Location of private keys')
@@ -65,129 +35,200 @@ def main():
     parser.add_argument('--prefix', default='', help='Specify a prefix to prepend to all host names')
     parser.add_argument('--private', action='store_true', help='Use private IP addresses (public are used by default)')
     parser.add_argument('--profile', help='Specify AWS credential profile to use')
-    parser.add_argument('--region', action='store_true', help='Append the region name at the end of the concatenation')
     parser.add_argument('--ssh-key-name', default='', help='Override the ssh key to use')
     parser.add_argument('--strict-hostkey-checking', action='store_true', help='Do not include StrictHostKeyChecking=no in ssh config')
-    parser.add_argument('--tags', help='A comma-separated list of tag names to be considered for concatenation. If omitted, all tags will be used')
+    parser.add_argument('--tags', action='append', default = [], help='A comma-separated list of tag names to be considered for concatenation. If omitted, all tags will be used.')
     parser.add_argument('--user', help='Override the ssh username for all hosts')
-    parser.add_argument('--white-list-region', default='', help='Which regions must be included. If omitted, all regions are considered', nargs='+')
+    parser.add_argument("--tag-filter", action='append', dest='tag_filters', default = [], help='tags to exclusively use for building config file')
+    parser.add_argument('--proxy-server-name', help='name of server to use a proxy')
+    parser.add_argument('--region', default='us-east-1', help='Which regions must be included. If omitted, all regions are considered')
+    parser.add_argument("--substitue", action='append', dest='words_to_substitue', default = [], help='word to substitue in tag value used to build host name')
+    parser.add_argument("--remove", action='append', dest='words_to_remove', default = [], help='word to remove from tag value used to build host name')
 
-    args = parser.parse_args()
+    return parser
 
-    instances = {}
-    counts_total = {}
-    counts_incremental = {}
-    amis = AMI_IDS_TO_USER.copy()
+def create_dict_of_filters(tag_and_values):
+    tag, values, *rest = tag_and_values.split("=")
+    values = values.split(",")
+    name_of_tag = 'tag:%s' % (tag,)
+    return {'Name': name_of_tag, 'Values': values}
 
-    print('# Generated on ' + time.asctime(time.localtime(time.time())))
-    print('# ' + ' '.join(sys.argv))
-    print('# ')
-    print('')
+def retreive_running_linux_instances(ec2, additional_filters = []):
+    required_filters = [{'Name': 'instance-state-name', 'Values': ['running']}]
+    additional_filters = [create_dict_of_filters(tag_and_values) for tag_and_values in additional_filters]
+    filters = required_filters + additional_filters
+    running_instances = ec2.instances.filter(Filters=filters)
+    return [instance for instance in running_instances if instance.platform != 'windows']
 
-    for region in boto.ec2.regions():
-        if args.white_list_region and region.name not in args.white_list_region:
-            continue
-        if region.name in BLACKLISTED_REGIONS:
-            continue
-        if args.profile:
-            conn = boto.ec2.connect_to_region(region.name, profile_name=args.profile)
-        else:
-            conn = boto.ec2.connect_to_region(region.name)
+def retrieve_proxy_server_by_name(ec2, name):
+    name_tag_and_value = "Name=%s" % (name,)
+    instances = retreive_running_linux_instances(ec2, [name_tag_and_value])
+    return next(iter(instances), None)
 
-        for instance in conn.get_only_instances():
-            if instance.state != 'running':
-                continue
+def get_tag_map(instance):
+    return {m['Key']: m['Value'] for m in instance.tags}
 
-            if instance.platform == 'windows':
-                continue
+def generate_host_name(instance, hostname_tags, substitution_list, remove_list, prefix):
 
-            if instance.key_name is None:
-                continue
+    tag_map = get_tag_map(instance)
 
-            if instance.launch_time not in instances:
-                instances[instance.launch_time] = []
+    tag_values_for_host_name = [tag_map[tag] for tag in hostname_tags]
+    host_name = '-'.join(tag_values_for_host_name).lower()
+    for word in remove_list: 
+        host_name = host_name.replace(word.lower(), '')
+    for sub in substitution_list:
+        split_sub = sub.split('=')
+        original = split_sub[0].lower()
+        replacement = split_sub[1].lower()
+        host_name = host_name.replace(original, replacement)
+    host_name = inflection.dasherize(host_name)
+    host_name = re.sub('([0-9]+)', r'-\1', host_name)
+    host_name = host_name.rstrip('-')
+    if prefix:
+        host_name = prefix + '-' + host_name
 
-            instances[instance.launch_time].append(instance)
+    return host_name
 
-            instance_id = generate_id(instance, args.tags, args.region)
+def generate_config_entry(instance, use_private_ip, no_identities_only, strict_hostkey_checking, hostname_tags, substitution_list, remove_list, prefix, proxy_server = None):
+    if use_private_ip and instance.private_ip_address:
+        ip_address = instance.private_ip_address
+    elif not use_private_ip and instance.public_ip_address:
+        ip_address = instance.public_ip_address
+    elif not use_private_ip and instance.private_ip_address:
+        ip_address = instance.private_ip_address
+    else:
+        sys.stderr.write('Cannot lookup ip address for instance %s' % instance.id)
+        return
 
-            if instance_id not in counts_total:
-                counts_total[instance_id] = 0
-                counts_incremental[instance_id] = 0
+    host_name = generate_host_name(instance, hostname_tags, substitution_list, remove_list, prefix)
 
-            counts_total[instance_id] += 1
+    if instance.id:
+        print('# ID:', instance.id)
+    print('Host', host_name)
+    print('    HostName', ip_address)
 
-            if args.user:
-                amis[instance.image_id] = args.user
-            else:
-                if not instance.image_id in amis:
-                    image = conn.get_image(instance.image_id)
+    if proxy_server:
+        print('    Proxycommand ssh ' + proxy_server + ' -W %h:%p')
 
-                    for ami, user in AMI_NAMES_TO_USER.items():
-                        regexp = re.compile(ami)
-                        if image and regexp.match(image.name):
-                            amis[instance.image_id] = user
-                            break
+    if not no_identities_only:
+        # ensure ssh-agent keys don't flood when we know the right file to use
+        print('    IdentitiesOnly yes')
+    if not strict_hostkey_checking:
+        print('    StrictHostKeyChecking no')
+        print()
 
-                    if instance.image_id not in amis:
-                        amis[instance.image_id] = args.default_user
-                        if args.default_user is None:
-                            image_label = image.name if image is not None else instance.image_id
-                            sys.stderr.write('Can\'t lookup user for AMI \'' + image_label + '\', add a rule to the script\n')
+def main():
 
-    for k in sorted(instances):
-        for instance in instances[k]:
-            if args.private:
-                if instance.private_ip_address:
-                    ip_addr = instance.private_ip_address
-            else:
-                if instance.ip_address:
-                    ip_addr = instance.ip_address
-                elif instance.private_ip_address:
-                    ip_addr = instance.private_ip_address
-                else:
-                    sys.stderr.write('Cannot lookup ip address for instance %s, skipped it.' % instance.id)
-                    continue
+    parser = build_argument_parser()
 
-            instance_id = generate_id(instance, args.tags, args.region)
+    arguments = parser.parse_args()
 
-            if counts_total[instance_id] != 1:
-                counts_incremental[instance_id] += 1
-                instance_id += '-' + str(counts_incremental[instance_id])
+    profile = arguments.profile
 
-            hostid = args.prefix + instance_id
-            hostid = hostid.replace(' ', '_') # get rid of spaces
+    if not profile:
+        print("Missing profile")
+        return
 
-            if instance.id:
-                print('# id: ' + instance.id)
-            print('Host ' + hostid)
-            print('    HostName ' + ip_addr)
+    region = arguments.region
 
-            try:
-                if amis[instance.image_id] is not None:
-                    print('    User ' + amis[instance.image_id])
-            except:
-                pass
+    if not region:
+        print("Missing region")
+        return
 
-            if args.keydir:
-                keydir = args.keydir
-            else:
-                keydir = '~/.ssh/'
+    if not arguments.tags:
+        print('Missing tags to use for HostName')
+        return
 
-            if args.ssh_key_name:
-                print('    IdentityFile ' + keydir + args.ssh_key_name + '.pem')
-            else:
-                key_name = AMI_IDS_TO_KEY.get(instance.image_id, instance.key_name)
+    try:
+        session = boto3.Session(region_name = region, profile_name = profile)
+    except ProfileNotFound:
+        print("Invalid profile provided: \"" + profile + "\". Check your ~/.aws/credentials file to confirm the name of your profile.")
+        return
 
-                print('    IdentityFile ' + keydir + key_name.replace(' ', '_') + '.pem')
+    ec2 = session.resource('ec2')
 
-            if not args.no_identities_only:
-                # ensure ssh-agent keys don't flood when we know the right file to use
-                print('    IdentitiesOnly yes')
-            if not args.strict_hostkey_checking:
-                print('    StrictHostKeyChecking no')
-            print('')
+    filters = arguments.tag_filters
 
+    instances = retreive_running_linux_instances(ec2, filters)
+
+    proxy_server_name = arguments.proxy_server_name
+
+    proxy_server = None
+
+    if proxy_server_name:
+        proxy_server = retrieve_proxy_server_by_name(ec2, proxy_server_name)
+
+    if proxy_server_name and not proxy_server:
+        print("Unable to find proxy server. Servers available are:")
+        potential_proxy_servers = retreive_running_linux_instances(ec2)
+        for instance in potential_proxy_servers:
+            tag_map = get_tag_map(instance)
+            if 'Name' in tag_map:
+                print('  * ' + tag_map['Name'])
+            else: 
+                print('  * ' + instance.id + ' (ERROR retrieving name. Displaying instance id)')
+        return
+
+    if arguments.user:
+        print('User ' + arguments.user)
+
+    if arguments.keydir:
+        keydir = arguments.keydir
+    else:
+        keydir = '~/.ssh/'
+
+    if arguments.ssh_key_name:
+        use_instance_key_name = False
+        print('IdentityFile ' + keydir + arguments.ssh_key_name)
+        print()
+
+    substitution_list = arguments.words_to_substitue
+
+    remove_list = arguments.words_to_remove
+
+    hostname_tags = arguments.tags
+
+    prefix = arguments.prefix
+
+    use_private_ip = arguments.private
+
+    no_identities_only = arguments.no_identities_only
+
+    strict_hostkey_checking = arguments.strict_hostkey_checking
+
+    if proxy_server:
+        proxy_host_name = generate_host_name(proxy_server, hostname_tags, substitution_list, remove_list, prefix)
+        generate_config_entry(proxy_server, use_private_ip, no_identities_only, strict_hostkey_checking, hostname_tags, substitution_list, remove_list, prefix)
+
+    for instance in instances:
+        generate_config_entry(instance, use_private_ip, no_identities_only, strict_hostkey_checking, hostname_tags, substitution_list, remove_list, prefix, proxy_host_name)
+
+        # if arguments.private and instance.private_ip_address:
+        #   ip_address = instance.private_ip_address
+        # elif not arguments.private and instance.public_ip_address:
+        #   ip_address = instance.public_ip_address
+        # elif not arguments.private and instance.private_ip_address:
+        #   ip_address = instance.private_ip_address
+        # else:
+        #   sys.stderr.write('Cannot lookup ip address for instance %s' % instance.id)
+        #   return
+
+        # host_name = generate_host_name(instance, hostname_tags, substitution_list, remove_list, prefix)
+
+        # if instance.id:
+        #   print('# ID:', instance.id)
+        # print('Host', host_name)
+        # print('    HostName', ip_address)
+
+        # if proxy_server:
+        #   print('    Proxycommand ssh -W %h:%p ' + generate_host_name(proxy_server, hostname_tags, substitution_list, remove_list, prefix))
+
+        # if not arguments.no_identities_only:
+        #   # ensure ssh-agent keys don't flood when we know the right file to use
+        #   print('    IdentitiesOnly yes')
+        # if not arguments.strict_hostkey_checking:
+        #   print('    StrictHostKeyChecking no')
+        #   print()
 
 if __name__ == '__main__':
     main()
